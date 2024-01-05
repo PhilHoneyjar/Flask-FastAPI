0d485cdbdc7f7ba9e98fe39a7d93b70f933d7a83
Philipp Medoev <phil.medoev@gmail.com>
2024-01-05 13:59:29 +0300

    Added hierarchy Categories -> Goods in Templates directory

--- lesson_1/app.py
+++ lesson_1/app.py
@@ -10,17 +10,32 @@ def index():
 
 @app.route('/clothing')
 def clothing():
-    return render_template('clothing.html')
+    return render_template('categories/clothing.html')
 
 
 @app.route('/shoes')
 def shoes():
-    return render_template('shoes.html')
+    return render_template('categories/shoes.html')
 
 
-@app.route('/jackets')
+@app.route('/clothing/jackets')
 def jacket():
-    return render_template('jackets.html')
+    return render_template('categories/clothing/jackets.html')
+
+
+@app.route('/clothing/trousers')
+def trousers():
+    return render_template('categories/clothing/trousers.html')
+
+
+@app.route('/shoes/boots')
+def boots():
+    return render_template('categories/shoes/boots.html')
+
+
+@app.route('/shoes/sneakers')
+def sneakers():
+    return render_template('categories/shoes/sneakers.html')
 
 
 if __name__ == '__main__':
