c2bcdf98674b00fb96a41c83c4ad34438f0a5c76
Philipp Medoev <phil.medoev@gmail.com>
2024-01-09 21:29:15 +0300

    refactored user session management for enhanced simplicity

--- lesson_2/app.py
+++ lesson_2/app.py
@@ -3,37 +3,34 @@ from flask import Flask, render_template, request, make_response, redirect, url_
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-
-@app.route('/welcome', methods=['POST'])
-def welcome():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
 
         response = make_response(redirect(url_for('greet')))
-        response.set_cookie('user', f'{name} - {email}')
+        response.set_cookie('username', name)
+        response.set_cookie('useremail', email)
+        return response
+    return render_template('index.html')
+
+
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        response = make_response(redirect(url_for('index')))
+        response.delete_cookie('username')
+        response.delete_cookie('useremail')
         return response
 
+    username = request.cookies.get('username')
+    useremail = request.cookies.get('useremail')
 
-@app.route('/greet')
-def greet():
-    user_cookie = request.cookies.get('user')
-    if user_cookie:
-        username = user_cookie.split(' - ')[0]
-        return render_template('welcome.html', username=username)
-    else:
+    if not (username and useremail):
         return redirect(url_for('index'))
 
-
-@app.route('/logout')
-def logout():
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie('user', '', expires=0)
-    return response
+    return render_template('welcome.html', username=username, useremail=useremail)
 
 
 if __name__ == '__main__':
